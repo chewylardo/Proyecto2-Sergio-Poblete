23f26983606be315bd2443e543d3460e32310aa2
chewylardo <127953611+chewylardo@users.noreply.github.com>
2023-06-06 12:48:50 -0400

    Lectura Basica en c++

--- Proyecto2 Sergio Poblete/Proyecto2 Sergio Poblete.cpp
+++ Proyecto2 Sergio Poblete/Proyecto2 Sergio Poblete.cpp
@@ -1,19 +1,22 @@
-// Proyecto2 Sergio Poblete.cpp : Este archivo contiene la función "main". La ejecución del programa comienza y termina ahí.
-//
 
 #include <iostream>
+#include <iomanip>
+#include <fstream>
+#include <sstream>
+#include <vector>
+#include <queue>
+#include <limits>
+#include <unordered_map>
+#include <string>
+using namespace std;
 
-int main()
+
+int main(int argc, char** argv)
 {
-    std::cout << "Hello World!\n";
+    
+    cout << "argc:\t" << argc;
+    cout << "\n";
+    cout << "argv:\t" << argv[0];
+   
+    return 0;
 }
-
-// Ejecutar programa: Ctrl + F5 o menú Depurar > Iniciar sin depurar
-// Depurar programa: F5 o menú Depurar > Iniciar depuración
-
-// Sugerencias para primeros pasos: 1. Use la ventana del Explorador de soluciones para agregar y administrar archivos
-//   2. Use la ventana de Team Explorer para conectar con el control de código fuente
-//   3. Use la ventana de salida para ver la salida de compilación y otros mensajes
-//   4. Use la ventana Lista de errores para ver los errores
-//   5. Vaya a Proyecto > Agregar nuevo elemento para crear nuevos archivos de código, o a Proyecto > Agregar elemento existente para agregar archivos de código existentes al proyecto
-//   6. En el futuro, para volver a abrir este proyecto, vaya a Archivo > Abrir > Proyecto y seleccione el archivo .sln
