b463c60cdcb71719ee07420173e0ad13cce9ffda
Lagnarokk <>
2023-06-07 09:49:54 -0400

    Cambios laboratorio 07-06

--- Proyecto2 Sergio Poblete/Proyecto2 Sergio Poblete.cpp
+++ Proyecto2 Sergio Poblete/Proyecto2 Sergio Poblete.cpp
@@ -11,15 +11,20 @@
 using namespace std;
 
 
-typedef struct Guardian {
+typedef struct guardian {
+
     string Name, auxPowerLevel, Village, MainMaster;
     int PowerLevel;
+    guardian newMainMaster();
+
 }Guardian;
 
 
 typedef struct ciudades {
     string Nombre;
+    //list<ciudades*>Vecinos;
     list<string>Vecinos;
+    list<ciudades>newVecinos;
 
 }Ciudades;
 
@@ -62,6 +67,40 @@ Ciudades crearC(string nombre,string vecino,list<Ciudades> auxlist) {
    
 
     return newCiudad;
+}
+
+void ListaDevecinos(list<Ciudades>misCiudades) {
+
+
+    list<Ciudades>::iterator it;
+    for (it = misCiudades.begin(); it != misCiudades.end(); it++)
+    {
+        for (auto const& i : it->Vecinos) {
+
+            //lista.push(llamar funcion)
+
+            cout << i << endl;
+        }
+
+    }
+
+
+
+}
+
+Ciudades buscarVecino(list<Ciudades>misCiudades,string vecino) {
+
+    Ciudades auxCity;
+
+    list<Ciudades>::iterator it;
+    for (it = misCiudades.begin(); it != misCiudades.end(); it++)
+    {
+        if (it->Nombre == vecino) {
+            
+            return *it;
+        }
+
+    }
 }
 
 int main(int argc, char** argv)
@@ -115,8 +154,18 @@ int main(int argc, char** argv)
         stringstream ss(mynewstring);
         getline(ss,ciudad, ',');
         getline(ss,vecino, ',');
+        list<Ciudades>::iterator it2;
+        for (it2 = misCiudades.begin(); it2 != misCiudades.end(); it2++)
+        {
+            if (it2->Nombre == ciudad) {
+                
+                it2->Vecinos.push_back(vecino);
 
+                continue;
+            }
+        }
         misCiudades.push_back(crearC(ciudad, vecino, misCiudades));
+        
     }
     inFile2.close();
 
@@ -125,18 +174,23 @@ int main(int argc, char** argv)
   
 
   
-    list<Ciudades>::iterator it2;
-    for (it2 = misCiudades.begin(); it2 != misCiudades.end(); it2++)
+    list<Ciudades>::iterator it3;
+    for (it3 = misCiudades.begin(); it3 != misCiudades.end(); it3++)
     {
-        if (it2->Nombre != "comodin") {
-            cout << "\n" << it2->Nombre << endl;
-            cout << "\n se conecta con " ;
-            for (auto const& i : it2->Vecinos) {
+        if (it3->Nombre != "comodin") {
+            cout << "\n" << it3->Nombre << endl;
+            cout << "\nse conecta con\n" ;
+            for (auto const& i : it3->Vecinos) {
                 cout << i << endl;
             }
         }
             
     }
+
+
+
+
+
 
     return 0;
 }
