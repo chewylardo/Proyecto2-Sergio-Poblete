27e599a9275be0042f709c21cb6d6dc45e8f91f9
chewylardo <127953611+chewylardo@users.noreply.github.com>
2023-06-10 19:18:34 -0400

    Viajes terminados

--- Proyecto2 Sergio Poblete/Proyecto2 Sergio Poblete.cpp
+++ Proyecto2 Sergio Poblete/Proyecto2 Sergio Poblete.cpp
@@ -7,6 +7,7 @@
 #include <queue>
 #include <limits>
 #include <unordered_map>
+#include<cstdlib>
 #include <string>
 using namespace std;
 
@@ -16,7 +17,8 @@ typedef struct guardian {
     string Name, auxPowerLevel, Village, MainMaster;
     int PowerLevel;
     list<guardian>misAprendices;
-    guardian newMainMaster();
+    list<guardian>Maestro;
+    string estado = "sin vencer";
 
 
 } Guardian;
@@ -29,15 +31,128 @@ typedef struct ciudades {
     list<Guardian>guardianes;
 
 
+
 }Ciudades;
+Guardian reeplazarGuardian(Guardian i) {
+
+    i.Name = "Angron";
+    int max;
+    srand((unsigned)time(NULL));
+    int min = 0;
+    list<Guardian>::iterator it;
+    for (it = i.Maestro.begin(); it != i.Maestro.end(); it++) {
+
+        max = it->PowerLevel;
+        cout <<"max: "<< max << endl;
+    }
+
+    list<Guardian>::iterator it2;
+    for (it2 = i.misAprendices.begin(); it2 != i.misAprendices.end(); it2++) {
+
+        if (it2->PowerLevel > min) {
+            min = it2->PowerLevel;
+            cout << "min: " << min << endl;
+        }
+
+    }
+   
+    int random = min + (rand() % (max));
+    cout << "soy random " << random << endl;
+    i.PowerLevel = random;
+
+    return i;
+}
+
+int entrenar() {
+
+    return 0;
+}
+string viajar(Guardian miGuardian, list<Ciudades>misCiudades) {
+
+    system("cls");
+    cout << "A que ciudad desea usted viajar" << endl;
+    list<string>normal;
+    list<string>alquimea;
+    Ciudades CiudadActual;
+    string voydeviaje;
+    cout << "\n" << endl;
+    for (auto const& i : misCiudades) {
+        if (i.Nombre == miGuardian.Village) {
+            CiudadActual = i;
+            cout <<"mi ciudad es "<< CiudadActual.Nombre << endl;
+        }
+
+    }
+    cout << "Viaje regular + 1 punto" << endl;
+    cout << "------------------------------" << endl;
+    for (auto const& i : CiudadActual.Vecinos) {
+        
+        cout << i << endl;
+        normal.push_back(i);
+      
+    }
+    
+    cout << "\n\n" << endl;
+    cout << "Viaje con alquimea -4 puntos" << endl;
+    cout << "------------------------------" << endl;
+    int aux = 0;
+    list<Ciudades>::iterator it2;
+    for (it2 = misCiudades.begin(); it2 != misCiudades.end(); it2++) {
+
+        for (auto const& i : CiudadActual.newVecinos) {
+            
+            if (it2->Nombre == i.Nombre) {
+                aux++;
+            }
+        }
+
+        if (it2->Nombre != "comodin" && aux == 0) {
+            cout << it2->Nombre << endl;
+            alquimea.push_back(it2->Nombre);
+        }
+    }
+
+    while(voydeviaje!="yaviaje"){
+        getline(cin,voydeviaje);
+        for (auto const& i : normal) {
+
+            if (voydeviaje == i) {
+                system("cls");
+                cout << "viajaste a " << i << " ganaste 1 punto" << endl;
+                cout << "\n\n" << endl;
+                return i;
+            }
+            
+        }
+        
+        for (auto const& i : alquimea) {
+
+            if (voydeviaje == i) {
+                system("cls");
+                cout << "viajaste a " << i << " perdiste 4 puntos" << endl;
+                cout << "\n\n" << endl;
+                return i;
+            }
+            
+        }
+
+        cout << "porfavor ingrese un valor valido" << endl;
+    }
+    
+    
+
+    return "test1";
+}
 
 void menuInicial(list<Guardian>Guardianes, list<Ciudades>misCiudades){
 
     Guardian miGuardian;
+    queue<string>recorrido;
     miGuardian.Name = "Nulo";
     cout << "Bienvenido a el juego" << endl;
     cout << "selecione un guardian con el que quiere jugar (escriba el nombre exactamente igual)"<<endl;
     cout << "\n" << endl;
+  
   
 
     list<Ciudades>::iterator it;
@@ -46,6 +161,7 @@ void menuInicial(list<Guardian>Guardianes, list<Ciudades>misCiudades){
         for (auto const& i : it->guardianes) {
 
             cout << i.Name << endl;
+            cout << i.auxPowerLevel << endl;
             
         }
            
@@ -56,33 +172,52 @@ void menuInicial(list<Guardian>Guardianes, list<Ciudades>misCiudades){
     
     while (miGuardian.Name == "Nulo") {
         cin >> buscar;
-        for (auto const& i : Guardianes) {
 
-            if (i.Name == buscar) {
+        list<Guardian>::iterator it;
+        for (it = Guardianes.begin(); it != Guardianes.end(); it++) {
 
-                miGuardian.Name = i.Name;
-                miGuardian.Village = i.Village;
-                miGuardian.MainMaster = i.MainMaster;
-                miGuardian.PowerLevel = i.PowerLevel;
+            if (buscar == it->Name) {
+                miGuardian.Name = it->Name;
+                miGuardian.Village = it->Village;
+                miGuardian.MainMaster = it->MainMaster;
+                miGuardian.PowerLevel = it->PowerLevel;
+                //cout << "original power level" << it->PowerLevel << endl;
+                *it=reeplazarGuardian(*it);
                 //añadir funcion para reparar el guardian clonado, debe revisar su maestro y aprendces para  que no sea mas debil que sus aprendices y no sea mas fuerte que su maestro
 
-                
+                //cout << it->Name << endl;
+                //cout << it->PowerLevel << endl;
             }
+        }
+        
+    }
+    
+
+
+    system("cls");
+    bool StormhartDefeated = false;
+    string selector1;
+    
+    while (StormhartDefeated == false) {
+        cout << "Jugando con " << miGuardian.Name << endl;
+        cout << "Poder Actual " << miGuardian.PowerLevel << endl;
+        cout << "Ciudad Actual " << miGuardian.Village << endl;
+        cout << "------------------------------------------------" << endl;
+        cout << "Quiere viajar o entrenar" << endl;
+        cout << "(escriba que prefiere)" << endl;
+        cin >> selector1;
+        if (selector1 == "viajar") {
+            miGuardian.Village=viajar(miGuardian, misCiudades);
+        }
+        if (selector1 == "entrenar") {
 
         }
     }
     
-    
 
 }
 
-void reeplazarGuardian(Guardian i) {
 
-    i.Name = "Angron";
-
-
-
-}
 
 
 Guardian crearG(string name,  string  village, string  mainMaster, int powerlevel) {
@@ -92,8 +227,8 @@ Guardian crearG(string name,  string  village, string  mainMaster, int powerleve
     newGuardian.Name = name;
     newGuardian.Village = village;
     newGuardian.MainMaster = mainMaster;
-    newGuardian.auxPowerLevel = powerlevel;
-
+    newGuardian.PowerLevel = powerlevel;
+   // newGuardian.PowerLevel = stoi(newGuardian.auxPowerLevel);
 
     return newGuardian;
 }
@@ -139,6 +274,7 @@ Ciudades buscarVecino(list<Ciudades>misCiudades, string vecino) {
 
     }
 
+    return *it;
 
 }
 
@@ -212,9 +348,10 @@ int main(int argc, char** argv)
         PowerLevel = stoi(auxPowerLevel);
         getline(ss, MainMaster, ',');
         getline(ss, Village, ',');
+       // cout << PowerLevel << endl;
        
       
-
+       
         Guardianes.push_back(crearG(Name, Village, MainMaster, PowerLevel));
         //cout << Village<< endl;
     }
@@ -225,7 +362,9 @@ int main(int argc, char** argv)
     for (it = Guardianes.begin(); it != Guardianes.end(); it++)
     {
         string name = it->Name;
-        //cout << name << endl;
+       // cout << name << endl;
+        //cout << it->PowerLevel << endl;
+        
     }
 
     ifstream inFile2;
@@ -327,7 +466,16 @@ int main(int argc, char** argv)
         for (it6 = Guardianes.begin(); it6 != Guardianes.end(); it6++) {
 
             if (it5->MainMaster == it6->Name){
-                it5->newMainMaster() = *it6;
+                it5->Maestro.push_back(*it6);
+            }
+
+        }
+
+        list<Guardian>::iterator it7;
+        for (it7 = Guardianes.begin(); it7 != Guardianes.end(); it7++) {
+
+            if (it7->MainMaster == it5->Name) {
+                it5->misAprendices.push_back(*it7);
             }
 
         }
